1b9ca7c95910f46a8f2430b718963e706f1a342f
QuocPham (kai) <anhchang1412@gmail.com>
2021-05-02 16:27:04 +0700

    Refactor: change Port heroku

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { addUser, removeUser } = require('./socket/user');
 
 Model.knex(knex);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
