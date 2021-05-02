04358eeba52ed64731885190b3c5a07a2f614bad
QuocPham (kai) <anhchang1412@gmail.com>
2021-05-02 16:38:24 +0700

    Refactor: change env heroku

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '.env' });
 
 module.exports = {
-  client: process.env.DB_HOST,
+  client: process.env.DB_TYPE,
   connection: {
+    host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
